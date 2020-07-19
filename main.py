7f1380db23917e1e27b6207a65736a56087199da
Sander Datema <mail@sanderdatema.nl>
2020-07-19 23:43:07 +0200

    Work on arguments

--- main.py
+++ main.py
@@ -22,10 +22,13 @@ settings.init()
 parser = argparse.ArgumentParser(description=f"-== PassengerFlying v{CURRENT_VERSION} ==-")
 parser.add_argument("--debug", help="increase output verbosity", action="store_true")
 parser.add_argument("--touchosc_device_ip", help="IP address of TouchOSC device", default="192.168.2.9")
-parser.add_argument("--touchosc_device_port", help="Port of TouchOSC device", default="5006")
-parser.add_argument("--touchosc_server_port", help="Port of TouchOSC server", default="5005")
+parser.add_argument("--touchosc_device_port", help="Port of TouchOSC device", default=5006)
+parser.add_argument("--touchosc_server_ip", help="IP address of TouchOSC server", default="0.0.0.0")
+parser.add_argument("--touchosc_server_port", help="Port of TouchOSC server", default=5005)
 parser.add_argument("aircraft", help="folder name of aircraft to load")
 args = parser.parse_args()
+
+# Save the arguments list to the global variables list
 settings.globalList["ARGS"] = args
 
 verbose_level = "DEBUG" if args.debug else "WARNING"
